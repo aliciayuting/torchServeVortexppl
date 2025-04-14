c8e0c552f0b15188d9e3bd60c00d4c3b8b7f10f8
TY373 <TY373@node4.gojiexp.goji-pg0.wisc.cloudlab.us>
2025-04-14 03:13:21 +0000

    log

--- launch_serve.sh
+++ launch_serve.sh
@@ -2,3 +2,4 @@ export TS_LOG_CONFIG=./log4j.properties
 torchserve --start --model-store model_store\
     --models monoflmr.mar --disable-token-auth\
     --ts-config config.properties\
+    --log-config ./log4j.properties
