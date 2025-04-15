e2bc1cb46dc8085c890c65e601ae17d80963eb69
Alicia <Alicia@node0.gojiexp.goji-pg0.wisc.cloudlab.us>
2025-04-15 02:49:15 +0000

    cd

--- ppl2/archieve_ppl2.sh
+++ ppl2/archieve_ppl2.sh
@@ -1,7 +1,8 @@
+EXTRA=$(find SenseVoice -type f | paste -sd "," -)
 torch-model-archiver \
   --model-name monospeech \
   --version 1.0 \
   --handler speech_handler.py \
-  --extra-files "speechRetrieve.py,$(find SenseVoice -type f | tr '\n' ',')" \
+  --extra-files "speechRetrieve.py,${EXTRA}" \
   --export-path model_store \
   --force
